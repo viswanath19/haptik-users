7bb9367a6903a2b5779cc4d7f546cc20986cd42d
ViswanathSarma <viswanath.sarma@acquire.io>
2021-09-17 18:45:23 +0530

    initial table data

--- src/App.js
+++ src/App.js
@@ -3,43 +3,60 @@ import './App.css';
 import Table from '../src/components/table';
 import React, { Component } from 'react'
 
-export class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       friendsList : [{
+         name:'Viswanath',
+         isFavourite:true,
+         isActive:true
+       },{
+         name:'Sreenivas',
+         isFavourite:true,
+         isActive:true
+       }],
+       addFriendName:''
+    }
+  }
+
+  addFriend = () => {
+    const friends = this.state.friendsList;
+    friends.push({
+      name:this.state.addFriendName,
+      isFavourite:false,
+      isActive:false
+    });
+    this.setState({friendsList:friends});
+  }
+  
   render() {
+    const {friendsList} = this.state;
     return (
       <div className="App">
       <header className="App-header">  
       <div>
+        <label htmlFor="fname" style={{fontSize:'13px'}}>Friend Name:</label>
+        <input type="text" placeholder="Enter Friend Name" name="fname" onChange={(e)=>{this.setState({addFriendName:e.target.value})}}/>
+        <button onClick={()=>this.addFriend()}>Add Friend</button>
         <h1>Your Friends List</h1>
         <br/>
         <Table bordered>
           <tbody>
-            <tr style={{borderBottom:'1px solid black'}}>
-              <td>
-                <div>
-                  <span>Bharat</span>
-                  <button style={{float:'right'}}>Delete</button>
-                  <button style={{float:'right'}}>Add Favourite</button>
-                </div>
-              </td>
-            </tr>
-            <tr style={{borderBottom:'1px solid black'}}>
-              <td>
-                <div>
-                  <span>Bharat</span>
-                  <button style={{float:'right'}}>Delete</button>
-                  <button style={{float:'right'}}>Add Favourite</button>
-                </div>
-              </td>
-            </tr>
-            <tr style={{borderBottom:'1px solid black'}}>
-              <td>
-                <div>
-                  <span>Bharat</span>
-                    <button style={{float:'right'}}>Delete</button>
-                    <button style={{float:'right'}}>Add Favourite</button>
-                </div>
-              </td>
-            </tr>
+            {friendsList.map((item,index)=>{
+              return (
+                <tr style={{borderBottom:'1px solid black'}} key={index}>
+                  <td>
+                    <div>
+                      <span>{item.name}</span>
+                      <button style={{float:'right'}}>Delete</button>
+                      <button style={{float:'right'}}>Add Favourite</button>
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </Table>
       </div>
